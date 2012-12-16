7979a8487a58896dc4a41f58298143a8b1b1109a
Eugene Howe <eugene@xtreme-computers.net>
2012-12-16 14:09:48 -0500

    default to gallery page 0 if no options passed

--- lib/imgur/requests/get_images.rb
+++ lib/imgur/requests/get_images.rb
@@ -2,14 +2,14 @@ class Imgur::Client
   class Real
     def get_images(params={})
       path = ""
-      path = path + "/#{params[:resource]}"
+      path = path + "/#{params[:resource] || "gallery"}"
       if params[:section]
         path = path + "/#{params[:section]}"
       elsif params[:subreddit]
         path = path + "/r/#{params[:subreddit]}"
       end
       path = path + "/#{params[:sort]}" if params[:sort]
-      path = path + "/#{params[:page]}.json"
+      path = path + "/#{params[:page] || 0}.json"
 
       request(
         :method => :get,
--- spec/images_spec.rb
+++ spec/images_spec.rb
@@ -4,7 +4,7 @@ describe "images" do
   let(:client) { create_client }
 
   it "should contain images" do
-    images = client.images.all(resource: "gallery", page: 0)
+    images = client.images.all
     images.should_not be_empty
     images.first.should be_a_kind_of(Imgur::Client::Image)
   end
